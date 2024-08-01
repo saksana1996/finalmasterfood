54ff977dc337c49bbd05bf772a2870faa7cfd8bd
50Mp <somphongchanthamixay01@gmail.com>
2024-08-01 21:29:56 +0900

    create account views

--- app/src/main/java/com/example/masterfood/MainActivity.kt
+++ app/src/main/java/com/example/masterfood/MainActivity.kt
@@ -6,42 +6,46 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.masterfood.ui.theme.MasterFoodTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MasterFoodTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text("Small Top App Bar")
+                            }
+                        )
+                    },
+                ) { innerPadding ->
+                    ScrollContent(innerPadding)
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+class ScrollContent(innerPadding: PaddingValues) {
+ va
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MasterFoodTheme {
-        Greeting("Android")
-    }
-}
